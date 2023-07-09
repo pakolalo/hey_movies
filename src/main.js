00614fd123d7a5a16d12fdab6415119889d775f9
Francisco del Collado <fedelcollado@gmail.com>
2023-07-08 18:15:58 -0600

    added axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+const api = axios.create({
+    baseURL:'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    params: {
+        'api_key':API_KEY,
+    },
+});
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/all/day?api_key='+ API_KEY);
-    const data = await res.json();
+    const {data} = await api('trending/all/day');
 
     const movies = data.results;
     movies.forEach(movie => {
@@ -20,8 +29,7 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getCategoriesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
+    const {data} = await api('genre/movie/list');
 
     const categories = data.genres;
     categories.forEach(category => {
